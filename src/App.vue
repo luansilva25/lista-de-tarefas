b19f09f52714fe105b0cbc79d2b39557092fdb66
luansilva25 <luansilva250807@gmail.com>
2023-12-07 00:39:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <adicionar />
+  </div>
+</template>
+<script>
+import adicionar from './components/adicionar.vue'
+export default {
+  components: { adicionar },
+  
+}
+</script>
+<style>
+    .container{
+      display: flex;
+      justify-content: space-between;
+      height: 100vh;
+      overflow-x: hidden;
+    }
+</style>
